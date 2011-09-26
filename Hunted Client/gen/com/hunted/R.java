0d6d6ab09cbbbb8496f627cac72cee005e738dca
mong <among0424@gmail.com>
2011-09-26 17:05:17 +0800

    Combine works from 新翔 into Hunted Client project

--- Hunted Client/gen/com/hunted/R.java
+++ Hunted Client/gen/com/hunted/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
+        public static final int blue=0x7f020016;
         public static final int botton=0x7f020000;
         public static final int botton2=0x7f020001;
         public static final int boy=0x7f020002;
@@ -20,6 +21,7 @@ public final class R {
         public static final int button_new_game2=0x7f020006;
         public static final int button_single_player=0x7f020007;
         public static final int button_single_player2=0x7f020008;
+        public static final int darkgray=0x7f020014;
         public static final int dialog_bg=0x7f020009;
         public static final int hubter=0x7f02000a;
         public static final int icon=0x7f02000b;
@@ -31,35 +33,44 @@ public final class R {
         public static final int start=0x7f020011;
         public static final int title_background=0x7f020012;
         public static final int touch=0x7f020013;
+        public static final int white=0x7f020015;
     }
     public static final class id {
-        public static final int button_start=0x7f060007;
-        public static final int cancel=0x7f06000a;
-        public static final int cancel2=0x7f060003;
-        public static final int editText1=0x7f060000;
-        public static final int editText2=0x7f060009;
-        public static final int icon=0x7f06000d;
-        public static final int icon2=0x7f06000f;
-        public static final int imageView1=0x7f060005;
-        public static final int mainLayout=0x7f06000c;
-        public static final int ok=0x7f060008;
-        public static final int ok2=0x7f060002;
-        public static final int relativeLayout1=0x7f060006;
-        public static final int text=0x7f06000e;
-        public static final int textView1=0x7f060004;
-        public static final int textView2=0x7f060001;
-        public static final int textView3=0x7f06000b;
+        public static final int button_start=0x7f060009;
+        public static final int cancel=0x7f06000c;
+        public static final int cancel2=0x7f060005;
+        public static final int editText1=0x7f060002;
+        public static final int editText2=0x7f06000b;
+        public static final int icon=0x7f06000f;
+        public static final int icon2=0x7f060011;
+        public static final int imageView1=0x7f060007;
+        public static final int mainLayout=0x7f06000e;
+        public static final int myMapView1=0x7f060001;
+        public static final int myTextView1=0x7f060000;
+        public static final int ok=0x7f06000a;
+        public static final int ok2=0x7f060004;
+        public static final int relativeLayout1=0x7f060008;
+        public static final int text=0x7f060010;
+        public static final int textView1=0x7f060006;
+        public static final int textView2=0x7f060003;
+        public static final int textView3=0x7f06000d;
     }
     public static final class layout {
-        public static final int host_dialog=0x7f030000;
-        public static final int join_game=0x7f030001;
-        public static final int joindialog=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int row=0x7f030004;
+        public static final int game=0x7f030000;
+        public static final int host_dialog=0x7f030001;
+        public static final int join_game=0x7f030002;
+        public static final int joindialog=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int row=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int str_err_address=0x7f040004;
+        public static final int str_err_location=0x7f040005;
+        public static final int str_latitude=0x7f040003;
+        public static final int str_longitude=0x7f040002;
+        public static final int str_my_location=0x7f040006;
     }
     public static final class style {
         public static final int MyDialog=0x7f050000;
